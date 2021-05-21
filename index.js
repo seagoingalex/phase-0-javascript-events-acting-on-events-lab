5c4c5af48e2e6ad859b39f25e06f2e5f220a61e5
Alex Calvert <alexanderajcalvert@gmail.com>
2021-05-21 08:33:00 -0700

    commit tree 0354e3df46343ecd8e018443682bef8d76f0771d

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
 // Your code here
+const dodger = document.getElementById("dodger");
+dodger.style.backgroundColor = "pink";
+
 function moveDodgerLeft() {
     const leftNumbers = dodger.style.left.replace("px", "");
     const left = parseInt(leftNumbers, 10);
@@ -15,11 +18,11 @@ document.addEventListener("keydown", function(e) {
 }); 
 
 function moveDodgerRight() {
-    const rightNumbers = dodger.style.left.replace("px", "");
-    const right = parseInt(rightNumbers,10);
+    const leftNumbers = dodger.style.left.replace("px", "");
+    const left = parseInt(leftNumbers,10);
 
-    if (right < 360) {
-        dodger.style.left = `${right + 1}px`;
+    if (left < 360) {
+        dodger.style.left = `${left + 1}px`;
     }
 }
 
